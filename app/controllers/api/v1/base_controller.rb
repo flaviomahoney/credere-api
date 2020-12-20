89d76c5b483ac0678df279ac3092be49d2a72239
flaviomahoney <flavio@mahoneyt.com>
2020-12-20 16:22:23 -0300

    Added some base controller information but with pundit now

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,16 +1,21 @@
 class Api::V1::BaseController < ActionController::API
-    after_action :verify_authorized, except: :index
-    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  include Pundit
 
-    private
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
 
-    def user_not_authorized(exception)
-        render json: {
-        error: "Unauthorized #{exception.policy.class.to_s.underscore.camelize}.#{exception.query}"
-        }, status: :unauthorized
-    end
+  rescue_from Pundit::NotAuthorizedError,   with: :user_not_authorized
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
-    def not_found(exception)
-        render json: { error: "Um movimento inválido foi detectado, infelizmente a sonda ainda não possui a habilidade de #vvv" }, status: :not_found
-    end
+  private
+
+  def user_not_authorized(exception)
+    render json: {
+      error: "Unauthorized #{exception.policy.class.to_s.underscore.camelize}.#{exception.query}"
+    }, status: :unauthorized
+  end
+
+  def not_found(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
 end
