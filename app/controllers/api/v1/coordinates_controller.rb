7ada97135aa4ec79a3f64feae7764695443714a9
flaviomahoney <flavio@mahoneyt.com>
2020-12-20 16:22:45 -0300

    Completed create for coordinates

--- app/controllers/api/v1/coordinates_controller.rb
+++ app/controllers/api/v1/coordinates_controller.rb
@@ -11,6 +11,17 @@ class Api::V1::CoordinatesController < Api::V1::BaseController
     end
 
     def result
+        @coordinate = Coordinate.find(params[:id])
+    end
 
-    end    
+    private
+
+    def coordinate_params
+        params.require(:coordinate).permit(:movimento)
+    end
+
+    def render_error
+        render json: { errors: "Um movimento inválido foi detectado, infelizmente a sonda ainda não possui a habilidade de #vvv" },
+        status: :unprocessable_entity
+    end
 end
