8bf18c939459058f7adfd90a9133cd50e0f94dd5
flaviomahoney <flavio@mahoneyt.com>
2020-12-20 16:15:58 -0300

    Already did the create element of the coordinate controller

--- app/controllers/api/v1/coordinates_controller.rb
+++ app/controllers/api/v1/coordinates_controller.rb
@@ -1,9 +1,16 @@
 class Api::V1::CoordinatesController < Api::V1::BaseController
     def create
-        
+        @coordinate = Coordinate.new(coordinate_params)
+        @coordinate.user = current_user
+        authorize @coordinate
+        if @coordinate.save
+            render :show, status: :created
+        else
+            render_error
+        end
     end
 
     def result
 
-    end
-  end
+    end    
+end
